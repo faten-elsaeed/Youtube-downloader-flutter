e675b226b8e2c29b54578171ddc2ea5d1522c7ae
fatenelsaeid281 <fatenelsaeed.01@gmail.com>
2024-11-08 23:22:37 +0200

    REFACTOR

--- lib/core/helpers/file_downloader.dart
+++ lib/core/helpers/file_downloader.dart
@@ -3,12 +3,12 @@ import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileDownloader {
-  Future<String> download(String url, String name) async {
+  Future<File> download(String url, String name) async {
     Directory directory = await getApplicationDocumentsDirectory();
     String path = '${directory.path}/$name';
     try {
       await Dio().download(url, path);
-      return path;
+      return File(path);
     } catch (e) {
       throw Exception("Failed to download image: $e");
     }
